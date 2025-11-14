1287e12b263b4080cb1d6ffe3c087068da52a9c4
matheusmansur10-dot <matheusmansur10@gmail.com>
2025-11-14 10:14:48 -0300

    Revert "Merge pull request #6 from matheusmansur10-dot/feature-cliente"
    This reverts commit f99a135fc51717c6d01d4d5c0ce1fb65734c148b, reversing
    changes made to 62cb9aba2d68a26278ab242b5a2b04b23762a7f7.

--- include/pedido.h
+++ include/pedido.h
@@ -3,19 +3,19 @@
 
 typedef struct
 {
-    int id;         // identificação do pedido
-    int clienteId;  // id do cliente
+    int id; // identificação do pedido
+    int clienteId; // id do cliente
     char data[11];  // data do pedido "dd/mm/aaaa"
-    double total;   // valor total do pedido
+    double total; // valor total do pedido
 } Pedido;
 
 
 typedef struct 
 {
-    int pedidoId;   // id do pedido ao qual o item pertence
-    int produtoId;  // id do produto comprado
+    int pedidoId;  // id do pedido ao qual o item pertence
+    int produtoId; // id do produto comprado
     int quantidade; // quantidade do produto
-    double subtotal;// valor (quantidade * preco do produto)
+    double subtotal; // valor (quantidade * preco do produto)
 } ItemPedido;
 
 int analisarPedido(int id);
